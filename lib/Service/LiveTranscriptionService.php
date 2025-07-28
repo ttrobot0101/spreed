018e7b9c2e1593127cfe8f117491db56036b86df
Daniel Calviño Sánchez <danxuliu@gmail.com>
2025-07-28 14:01:12 +0200

    feat: Add endpoint to enable and disable live transcriptions
    The endpoint just forwards the request to the external app
    "live_transcription", but using a standard Talk endpoint makes possible
    to abstract that from the clients.
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- lib/Service/LiveTranscriptionService.php
+++ lib/Service/LiveTranscriptionService.php
@@ -9,18 +9,28 @@ declare(strict_types=1);
 namespace OCA\Talk\Service;
 
 use \RuntimeException;
+use OCA\Talk\Participant;
+use OCA\Talk\Room;
 use OCP\App\IAppManager;
+use OCP\Http\Client\IResponse;
+use OCP\IUserManager;
+use Psr\Log\LoggerInterface;
 
 class LiveTranscriptionService {
 
 	public function __construct(
+		private ?string $userId,
 		private IAppManager $appManager,
+		private IUserManager $userManager,
+		protected LoggerInterface $logger,
 	) {
 	}
 
-	public function isLiveTranscriptionAppEnabled(): bool {
+	public function isLiveTranscriptionAppEnabled(?object $appApiPublicFunctions = null): bool {
 		try {
-			$appApiPublicFunctions = $this->getAppApiPublicFunctions();
+			if ($appApiPublicFunctions ===  null) {
+				$appApiPublicFunctions = $this->getAppApiPublicFunctions();
+			}
 		} catch (\RuntimeException $e) {
 			return false;
 		}
@@ -45,5 +55,76 @@ class LiveTranscriptionService {
 		}
 
 		return $appApiPublicFunctions;
+	}
+
+	/**
+	 * @throws RuntimeException if the external app "live_transcription" is not
+	 *         available, or if the request failed.
+	 */
+	public function enable(Room $room, Participant $participant): void {
+		$params = [
+			'roomToken' => $room->getToken(),
+			'sessionId' => $participant->getSession()->getSessionId(),
+			'enable' => true,
+		];
+
+		$this->requestToExAppLiveTranscription('/transcribeCall', $params);
+	}
+
+	/**
+	 * @throws RuntimeException if the external app "live_transcription" is not
+	 *         available, or if the request failed.
+	 */
+	public function disable(Room $room, Participant $participant): void {
+		$params = [
+			'roomToken' => $room->getToken(),
+			'sessionId' => $participant->getSession()->getSessionId(),
+			'enable' => false,
+		];
+
+		$this->requestToExAppLiveTranscription('/transcribeCall', $params);
+	}
+
+	/**
+	 * @throws RuntimeException if the external app "live_transcription" is not
+	 *         available, or if the request failed.
+	 */
+	private function requestToExAppLiveTranscription(string $route, array $params): IResponse {
+		try {
+			$appApiPublicFunctions = $this->getAppApiPublicFunctions();
+		} catch (RuntimeException $e) {
+			if ($e->getMessage() === 'app-api') {
+				$this->logger->error('AppAPI is not enabled');
+			} else if ($e->getMessage() === 'app-api-functions') {
+				$this->logger->error('Could not get AppAPI public functions', ['exception' => $e]);
+			}
+			throw $e;
+		}
+
+		if (!$this->isLiveTranscriptionAppEnabled($appApiPublicFunctions)) {
+			$this->logger->error('External app live_transcription is not enabled');
+			throw new RuntimeException('live-transcription-app');
+		}
+
+		$response = $appApiPublicFunctions->exAppRequest(
+			'live_transcription',
+			$route,
+			$this->userId,
+			'POST',
+			$params,
+		);
+
+		if (is_array($response) && isset($response['error'])) {
+			$this->logger->error('Request to external app live_transcription failed: ' . $response['error']);
+			throw new RuntimeException('request');
+		}
+		if (is_array($response)) {
+			// AppApi only uses array responses for errors, so this should never
+			// happen.
+			$this->logger->error('Request to external app live_transcription failed: response is not a valid response object');
+			throw new RuntimeException('response');
+		}
+
+		return $response;
 	}
 }
